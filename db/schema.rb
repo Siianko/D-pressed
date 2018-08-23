4957d94414b12c413d688c0091e1d406612d5519
Siianko <tsiyanko@gmail.com>
2018-08-23 10:14:33 +0200

    conflicts fix, attempt 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_08_17_075318) do
-=======
 ActiveRecord::Schema.define(version: 2018_08_21_103057) do
->>>>>>> 3ff69e9ad34edaaca40727de3a1d3af3b7413561
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_103057) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
